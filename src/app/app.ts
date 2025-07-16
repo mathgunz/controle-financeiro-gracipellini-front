ede0d460c313afdbbad5219862db0532a659e669
matheus <mathgunz@gmail.com>
2025-07-16 18:35:21 -0300

    ndo

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { provideNgxMask } from 'ngx-mask';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterModule],
   template: `<router-outlet></router-outlet>`,
+  providers: [ provideNgxMask() ]
 })
 export class App {}
--- src/app/nova-conta/nova-conta.ts
+++ src/app/nova-conta/nova-conta.ts
@@ -1,12 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { NgxMaskDirective, NgxMaskPipe } from 'ngx-mask';
 
 @Component({
   selector: 'app-nova-conta',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, NgxMaskDirective, NgxMaskPipe],
   templateUrl: './nova-conta.html',
   styleUrl: './nova-conta.css'
 })
@@ -19,11 +20,13 @@ export class NovaConta {
     }
   }
 
+  @ViewChild('inputData') inputData!: ElementRef<HTMLInputElement>;
+
   irParaNovaConta() {
     this.router.navigate(['/nova-conta']);
   }
 
-    @Input() novaConta = {
+  @Input() novaConta = {
     nome: '',
     valor: null,
     data: new Date(),
@@ -35,14 +38,29 @@ export class NovaConta {
     repetir: '',
     qtd: null,
     juros: null,
-    lembrete: false
+    lembrete: false,
+    dataTexto: ''
   };
-  
+
   abrirCalendario() {
-    // lógica opcional para abrir datepicker se for implementado depois
+    if (this.inputData) {
+      this.inputData.nativeElement.click();
+      console.warn('inputData encontrado');
+    } else {
+      console.warn('inputData não encontrado');
+    }
   }
 
+  ngAfterViewInit() {
+    // só para confirmar que a ref foi capturada
+    console.log('inputData:', this.inputData);
+  }
 
+  get dataComoDate(): Date | null {
+    const [dia, mes, ano] = this.novaConta.dataTexto?.split('/') || [];
+    if (!dia || !mes || !ano) return null;
+    return new Date(+ano, +mes - 1, +dia);
+  }
 }
 
 export class NovaContaComponent {
@@ -58,6 +76,7 @@ export class NovaContaComponent {
     repetir: '',
     qtd: null,
     juros: null,
-    lembrete: false
+    lembrete: false,
+    dataTexto: ''
   };
 }
